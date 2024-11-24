8cb187cc39baf78aeeecfe298b46585d54624301
Gadiel-Monteabaro <silvagadiel96@gmail.com>
2024-11-24 14:18:27 -0300

    proyecto carrito

---
+++ React/Proyectos-Nuevos/carrito-app/src/App.tsx
@@ -0,0 +1,27 @@
+function App() {
+  return (
+    <div className="container">
+      <div className="cards">
+        <div className="card">
+          <h2 className="card-title">Productos</h2>
+          <p className="card-">Arroz</p>
+          <p>Arroz</p>
+          <p>Arroz</p>
+          <p>Arroz</p>
+        </div>
+        <div className="card">
+          <h2 className="card-title">Compra</h2>
+          <div>
+            {" "}
+            <p className="card-">Arroz</p>
+            <p>Arroz</p>
+            <p>Arroz</p>
+            <p>Arroz</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
