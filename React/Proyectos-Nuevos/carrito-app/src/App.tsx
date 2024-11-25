de25e702c7924d1f41a14cdc3bea1efc512f051a
Gadiel-Monteabaro <silvagadiel96@gmail.com>
2024-11-24 22:49:18 -0300

    second-card grid layout css

--- React/Proyectos-Nuevos/carrito-app/src/App.tsx
+++ React/Proyectos-Nuevos/carrito-app/src/App.tsx
@@ -3,8 +3,9 @@ function App() {
     <div className="container">
       <div className="cards">
         <div className="card"></div>
-        <div className="card">
-          <div></div>
+        <div className="card second-card">
+          <section></section>
+          <footer className="footer">Gadiel Monteabaro</footer>
         </div>
       </div>
     </div>
