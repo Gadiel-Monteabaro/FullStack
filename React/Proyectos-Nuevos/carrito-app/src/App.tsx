f8f58422ab2149ad80e1b916c986f6440289423d
Gadiel-Monteabaro <silvagadiel96@gmail.com>
2024-11-25 20:19:01 -0300

    contenedor de productos, comentarios, bg-variables

--- React/Proyectos-Nuevos/carrito-app/src/App.tsx
+++ React/Proyectos-Nuevos/carrito-app/src/App.tsx
@@ -3,7 +3,17 @@ function App() {
     <div className="container">
       <div className="cards">
         <div className="card first-card">
-          <div className="border"></div>
+          <h2 className="card-title">Productos</h2>
+          <button className="product-box">
+            <img src="product.png" alt="imagen del producto" />
+            <p>Product N°1 </p>
+            <p>$15</p>
+          </button>
+          <button className="product-box">
+            <img src="product.png" alt="imagen del producto" />
+            <p>Product N°1</p>
+            <p>$15</p>
+          </button>
         </div>
         <div className="card second-card">
           <section></section>
